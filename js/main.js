f6e3e60d7e50e85a49d5398792e0a8abf262c266
Vitalii <vetal.ermlenk@gmail.com>
2022-11-08 22:45:22 +0200

    add block What People Say About theHam

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 
-
+// Service Menu
 const servicesMenu = document.querySelector('.second-page-service-menu-content');
 const servicesMenuContent = document.querySelector('.second-page-service-opinion-content').children;
+const downloadBtn = document.querySelector('.our-work-load-button');
 let index = 0;
 
 servicesMenu.addEventListener('click', (event) =>{
@@ -30,14 +31,13 @@ function activeContent (index, array){
 const workMenu = document.querySelector('.our-work-list');
 const workMenuImgItem =document.querySelectorAll(".our-work-img-item");
 
-console.log(workMenuImgItem);
-
 function filterImg (name){
     for (item of workMenuImgItem) {
         item.style.display ='none'; 
     }
+    downloadBtn.style.opacity = '1';
+    let i = 0;
     if(name === 'All'){
-        let i = 0;
         for (item of workMenuImgItem) {
                 item.style.display ='block'; 
                 i++;
@@ -47,14 +47,22 @@ function filterImg (name){
         }
             return;
         }
-
-    for (let i = 0; i < workMenuImgItem.length; i++) {
-        if(name === workMenuImgItem[i].dataset.index){
-            workMenuImgItem[i].style.display ='block';
+    for (let index = 0; i < workMenuImgItem.length; index++) {
+        if(name === workMenuImgItem[index].dataset.index){
+            workMenuImgItem[index].style.display ='block';
+            i++;
+            if(i === 12){
+                downloadBtn.style.opacity = '1';
+                return;
+            }
+            if(i<12){
+                downloadBtn.style.opacity = '0';
+            }
         }
+
     }
 }
-
+filterImg ('All');
 workMenu.addEventListener('click', (event) =>{
     for(tab of workMenu.children){
         tab.classList.remove('active-item');     
@@ -65,3 +73,75 @@ workMenu.addEventListener('click', (event) =>{
 
 
 
+downloadBtn.addEventListener('click', () => {
+    flag = 24
+    loader.classList.add(active)   
+    downloadBtn.style.opacity = '0';
+    setTimeout(function(){ workBlog.forEach((i)=>{
+        i.classList.add(active)
+        i.classList.remove(disable)
+    })
+    amazingTabs.forEach((elem)=>{
+        if(elem.dataset.article === 'all'){
+            elem.classList.add(active)
+        }else{
+            elem.classList.remove(active)
+        }
+        
+    })
+    loader.classList.remove(active)
+    loader.classList.add(disable)
+    }, 3000)
+})
+
+// say abou us
+
+const clientDots =document.querySelector('.say-about-us-dots');
+const clientDot =document.querySelectorAll('.say-about-us-img-dot');
+const clientComment = document.querySelectorAll('.say-about-us-item');
+
+const prevBtn = document.querySelector('.prev-arrow');
+const nextBtn = document.querySelector('.next-arrow');
+
+number = 0;
+clientDots.addEventListener('click', clickDots); 
+function clickDots(event){    
+    for(let a = 0; a < clientDot.length; a++){
+        clientDot[a].classList.remove('active');
+    }
+   event.target.classList.add('active');
+    for (let i = 0; i < clientComment.length; i++) {
+       if(event.target.dataset.index === clientComment[i].dataset.index){
+           number = i;
+       }
+    }
+    activeContent (number, clientComment);
+}
+
+
+
+prevBtn.addEventListener('click',() =>{ 
+    if (number === 0 ){
+        number=clientComment.length-1;
+    } else{
+        number--;
+    }
+    for(let a = 0; a < clientDot.length; a++){
+        clientDot[a].classList.remove('active');
+    }
+    clientDot[number].classList.add('active');
+    activeContent(number, clientComment);
+})
+nextBtn.addEventListener('click',() =>{
+    if (number === clientComment.length-1){
+        number= 0;
+    } else{
+        number++;
+    }
+    for(let a = 0; a < clientDot.length; a++){
+        clientDot[a].classList.remove('active');
+    }
+    clientDot[number].classList.add('active');
+    activeContent(number, clientComment);
+    
+})
